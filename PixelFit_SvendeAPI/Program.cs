42fe0cd20d6f00e8230145f3109730c8ad654e5c
MRasch1 <mrasch1102@gmail.com>
2026-08-13 10:30:40 +0200

    test3

--- PixelFit_SvendeAPI/Program.cs
+++ PixelFit_SvendeAPI/Program.cs
@@ -114,7 +114,7 @@ app.UseSwaggerUI();
 // Health check endpoint
 app.MapGet("/health", () => Results.Ok("Healthy"));
 
-//test2
+//test3
 
 // Must run before calls that rely on scheme/IP
 app.UseForwardedHeaders();
